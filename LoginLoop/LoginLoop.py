e1ab814f4418ef497e305260d8bdf6563a459230
Jordan Jalles <jordanjalles@gmail.com>
2015-08-31 14:33:18 -0700

    added starting and ending versions of LoginLoop

--- LoginLoop/LoginLoop.py
+++ LoginLoop/LoginLoop.py
@@ -1,5 +1,48 @@
-#Login Loop
+#Login Loop Template
 
-print("Enter the password: ")
-response = raw_input()
-print(response)
+password = "secret"
+user_response = ""
+message = ""
+
+user_response = raw_input("Password:")
+
+if (user_response == password):
+    message = """
+########## Access Granted ############
+"""
+else:
+    message = """
+########## Password Incorrect ##########
+########## Access Denied ###############
+"""
+print(message)
+
+
+
+
+###Complete Program
+##
+##password = "secret"
+##user_response = ""
+##message = ""
+##
+##while (user_response != password and user_response != "exit"):
+##    user_response = raw_input("Password:")
+##
+##    if (user_response == password):
+##        message = """
+##    ########## Access Granted ############
+##    """
+##    elif (user_response == "exit"):
+##        message = "Goodbye..."
+##    else:
+##        message = """
+##    ########## Password Incorrect ##########
+##    ########## Access Denied ###############
+##
+##    Please try again or type "exit"
+##    """
+##    print(message)
+##
+
+    
